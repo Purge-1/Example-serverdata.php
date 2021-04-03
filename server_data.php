dd0c50f756dfb8833f1031389f31fd15d9d464e9
Purge-1 <80547719+Purge-1@users.noreply.github.com>
2021-04-03 09:48:18 +0300

    Add files via upload

---
+++ server_data.php
@@ -0,0 +1,11 @@
+server|your vps ip
+port|your UDP PORT (EX: 17091)
+type|1
+#maint|server_data.php made by Purge
+
+beta_server|VPS IP
+beta_port|Your UDP Port (EX: 17091)
+
+beta_type|1
+meta|localhost
+RTENDMARKERBS1001
